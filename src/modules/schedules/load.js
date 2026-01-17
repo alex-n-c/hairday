94fee783cbc6825c9e31d760f215417ce49a1f6d
Alex <alexnobrecruz@yahoo.com.br>
2026-01-16 23:54:04 -0300

    Ajustes

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,8 +1,19 @@
+import { scheduleFetchByDay } from "../../services/schelude-fetch-by-id.js"
+import { schedulesShow } from "../schedules/show.js"
 import { hoursLoad } from "../form/hours-load.js"
 
 const selectedDate = document.getElementById("date")
 
-export function scheduleDays(){
+export async function scheduleDays(){
+    //Obtem a data do input
     const date = selectedDate.value
+
+    //Busca os agendamentos do dia
+    const dailySchedules = await scheduleFetchByDay({date})
+
+    //Exibe os agendamentos
+    schedulesShow({ dailySchedules})
+
+    //Renderiza as horas disponiveis
     hoursLoad({date})
 }
